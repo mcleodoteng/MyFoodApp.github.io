f9cfa7d6d58768e3d3a4043df13fa82f5e5331d0
mcleodoteng <mcleod.oteng5@gmail.com>
2023-12-04 13:04:37 +0000

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/FoodShop.github.io",
+  base: "/MyFoodApp.github.io",
   plugins: [react()],
 });
